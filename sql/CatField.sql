e4a351d67520a2ffb0a22019e76718b4e0c89291
avatarkorraa <e.deluca20@studenti.unisa.it>
2024-02-18 14:24:58 +0100

    Aggiunta di CatField.sql.

---
+++ sql/CatField.sql
@@ -0,0 +1,81 @@
+drop database if exists RifugioGatti;
+create database RifugioGatti;
+use RifugioGatti;
+
+create table Dipendente(
+
+	matricola varchar(100) primary key,
+    stipendio float not null,
+    ore int not null,
+    quotaOraria int not null
+
+);
+
+create table Volontario(
+
+	matricola varchar(100) primary key,
+    punti int not null,
+    ore int not null,
+    quotaOraria int not null
+
+);
+
+create table Buono(
+
+	codiceBuono varchar(100) primary key,
+    dataScadenza date not null,
+    dataErogazione date not null,
+    matricola varchar(100) not null,
+    quota int not null,
+    
+    foreign key (matricola) references Volontario(matricola) on delete cascade
+
+);
+
+create table Gatto(
+	
+    codiceidentificativounico varchar(100) primary key,
+    dataAdozione date,
+    età int not null,
+    razza varchar(100) not null,
+    genere varchar(100) not null,
+    matricolaDipendente varchar(100) not null,
+    matricolaVolontario varchar(100) not null,
+    
+    foreign key (matricolaDipendente) references Dipendente(matricola) on delete cascade,
+    foreign key (matricolaVolontario) references Volontario(matricola) on delete cascade
+
+);
+
+create table Veterinario(
+
+	matricolaMedica varchar(100) primary key,
+    nome varchar(100) not null,
+    cap varchar(100) not null,
+    via varchar(100) not null,
+    cognome varchar(100) not null,
+    citta varchar(100) not null
+
+);
+
+create table Visita(
+
+	id int auto_increment primary key,
+    dataVisita date not null,
+    codiceidentificativounico varchar(100) not null,
+    matricolaMedica varchar(100) not null,
+    
+    foreign key (matricolaMedica) references Veterinario(matricolaMedica) on delete cascade,
+    foreign key (codiceidentificativounico) references Gatto(codiceidentificativounico) on delete cascade
+
+);
+
+create table numeroTelefono(
+
+	numero varchar(100) not null,
+	id int auto_increment primary key,
+    matricolaMedica varchar(100) not null,
+    
+	foreign key (matricolaMedica) references Veterinario(matricolaMedica) on delete cascade
+    
+);
